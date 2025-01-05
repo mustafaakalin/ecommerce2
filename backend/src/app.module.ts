c0c8a62ff99ad580cfa88108a07fc80ad8f76522
𝖒𝖚𝖘𝖙𝖆𝖋𝖆 <121257754+mustafaakalin@users.noreply.github.com>
2025-01-05 16:33:31 +0300

    add entity imports to app module for comprehensive database integration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -22,6 +22,26 @@ import { SoldoutsModule } from './soldouts/soldouts.module';
 import { TestimonialsModule } from './testimonials/testimonials.module';
 import { UsersModule } from './users/users.module';
 import { OrderItemsModule } from './order_items/order_items.module';
+import { Address } from './addresses/entities/address.entity';
+import { Brand } from './brands/entities/brand.entity';
+import { Campaign } from './campaigns/entities/campaign.entity';
+import { Cart } from './carts/entities/cart.entity';
+import { Category } from './categories/entities/category.entity';
+import { Comment } from './comments/entities/comment.entity';
+import { Contact } from './contacts/entities/contact.entity';
+import { Coupon } from './coupons/entities/coupon.entity';
+import { Faq } from './faqs/entities/faq.entity';
+import { Like } from './likes/entities/like.entity';
+import { Order } from './orders/entities/order.entity';
+import { OrderItem } from './order_items/entities/order_item.entity';
+import { Product } from './products/entities/product.entity';
+import { Rating } from './ratings/entities/rating.entity';
+import { Shipment } from './shipments/entities/shipment.entity';
+import { Setting } from './settings/entities/setting.entity';
+import { Soldout } from './soldouts/entities/soldout.entity';
+import { Testimonial } from './testimonials/entities/testimonial.entity';
+import { User } from './users/entities/user.entity';
+
 
 @Module({
   imports: [AddressesModule, BrandsModule, CampaignsModule, CartsModule, CategoriesModule, CommentsModule, ContactsModule, CouponsModule, FaqsModule, LikesModule, OrdersModule, RatingsModule, ProductsModule, ShipmentsModule, SettingsModule, SoldoutsModule, TestimonialsModule, UsersModule, OrderItemsModule, 
@@ -39,7 +59,8 @@ import { OrderItemsModule } from './order_items/order_items.module';
         username: configService.get('DB_USERNAME'),
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_DATABASE'),
-        entities: [__dirname + '/**/**/*.entity{.ts,.js}'],
+        // entities: [__dirname + '/**/**/*.entity{.ts,.js}'],
+        entities: [Address, Brand, Campaign, Cart, Category, Comment, Contact, Coupon, Faq, Like, Order, OrderItem, Product, Rating, Shipment, Setting, Soldout, Testimonial, User],
         synchronize: configService.get('NODE_ENV') === 'development',
       }),
       inject: [ConfigService],
